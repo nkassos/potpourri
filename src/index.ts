120e352f659666b10ce4a74409e30ecaa95a85ff
Noah Kassos <noah.kassos@coupa.com>
2021-11-05 13:53:11 -0700

    Revert "Adding GraphUtil to exports"
    This reverts commit e8aa6f336cf5080a2bc62e0287ed815388d932ee.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { LinkedList } from './List/LinkedList';
 export { LinkedStack } from './Stack/LinkedStack';
 export { StringGraph } from './Graph/StringGraph';
-export { GraphUtil } from './Graph/GraphUtil';
